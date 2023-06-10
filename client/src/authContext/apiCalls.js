5d3c2af59197a98a073eebce4f1636316a50c8d0
KhatriPooja210 <khatripooja210@gmail.com>
2023-06-10 18:23:40 +0530

    update

--- client/src/authContext/apiCalls.js
+++ client/src/authContext/apiCalls.js
@@ -3,10 +3,14 @@ import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
 
 export const login = async (user, dispatch) => {
   dispatch(loginStart());
+  console.log(user)
   try {
-    const res = await axios.post("/auth/login", user);
+    const res = await axios.post("http://localhost:8800/api/auth/login", user);
+
+  
     dispatch(loginSuccess(res.data));
   } catch (err) {
+    console.log(err)
     dispatch(loginFailure());
   }
 };
